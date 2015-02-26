f33e33e89c232d271c512a4762bac9e3bc7b1dd5
Joe Cellucci <celluj34@gmail.com>
2015-02-25 21:28:33 -0600

    add settings provider for all app settings

---
+++ SharpMod.Node/providers/settingsProvider.js
@@ -0,0 +1,38 @@
+﻿SettingsProvider = function(diskDb) {
+	this.database = diskDb;
+};
+
+SettingsProvider.prototype.database = null;
+
+SettingsProvider.prototype.Username = function() {
+	var findOne = this.database.settings.findOne({Key: "Username"});
+	return findOne || "";
+};
+
+SettingsProvider.prototype.Password = function() {
+	var findOne = this.database.settings.findOne({Key: "Password"});
+	return findOne || "";
+};
+
+SettingsProvider.prototype.saveLogin = function(username, password, callback) {
+	if(!username || 0 === username.length) {
+		callback("Username must not be null.");
+	}
+
+	if(!password || 0 === password.length) {
+		callback("Password must not be null.");
+		return;
+	}
+
+	var options = {
+		multi: false,
+		upsert: true
+	};
+
+	this.database.settings.update({Key: "Username", Value: username}, options);
+	this.database.settings.update({Key: "Password", Value: password}, options);
+
+	callback(null);
+};
+
+exports.SettingsProvider = SettingsProvider;

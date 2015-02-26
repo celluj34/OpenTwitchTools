07eeabb40026ecb11156405789574a3644a7a062
Joe Cellucci <celluj34@gmail.com>
2015-02-25 22:08:38 -0600

    setup initial settings provider and move routes to app.js

--- SharpMod.Node/app.js
+++ SharpMod.Node/app.js
@@ -1,14 +1,15 @@
 ﻿var express = require("express");
-var routes = require("./routes");
 var http = require("http");
 var path = require("path");
 var diskdb = require("diskdb");
 var irc = require("irc");
+var settingsProvider = require("./providers/settingsProvider.js").SettingsProvider;
 
 var app = express();
 
 // all environments
 app.set("views", path.join(__dirname, "views"));
+app.set("env", "development");
 app.set("view engine", "jade");
 app.use(express.favicon());
 app.use(express.logger("dev"));
@@ -24,10 +25,31 @@ if("development" == app.get("env")) {
 	app.use(express.errorHandler());
 }
 
-app.get("/", routes.index);
-app.get("/login", routes.login);
+diskdb = diskdb.connect("./sharpdb", ["settings"]);
 
-diskdb.connect("sharpdb", ["settings"]);
+var settingsProvider = new SettingsProvider(diskdb);
+
+app.get("/", function (req, res) {
+	res.render("login", {
+		"Username": settingsProvider.Username(),
+		"Password": settingsProvider.Password()
+	});
+});
+
+app.post("/", function(req, res) {
+	settingsProvider.saveLogin(req.param("username"), req.param("password"), function(error) {
+		if(error) {
+			res.redirect("/", error);
+		}
+		else {
+			res.redirect("/chat");
+		}
+	});
+});
+
+app.get("/chat", function(req, res) {
+	res.render("chat");
+});
 
 //var client = irc.Client("irc.twitch.tv", {
 //	userName: "nodebot",

9356866db8e5b3c46f0dde96fedf0550a172ab7c
Joe Cellucci <celluj34@gmail.com>
2015-02-27 16:55:49 -0600

    add chat javascript file

---
+++ SharpMod.Node/public/javascripts/chat.js
@@ -0,0 +1,17 @@
+﻿$(function() {
+	var serverBaseUrl = document.domain;
+
+	var socket = io.connect(serverBaseUrl + ":18044");
+
+	socket.on("incomingMessage", function(data) {
+		var message = data.message;
+		var name = data.name;
+		var msg = "<b>" + name + "</b><br />" + message;
+		console.log(msg);
+		$("#messages").prepend(msg + "<hr />");
+	});
+
+	$(window).on("beforeunload", function() {
+		socket.close();
+	});
+});

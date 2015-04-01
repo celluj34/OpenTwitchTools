f4d6a2f5c1c919cbb6fa85fb6a79d530d2e4bb7c
Joe Cellucci <celluj34@gmail.com>
2015-03-31 22:21:52 -0500

    add a few more options for the atom-shell

--- SharpMod.Node/app.js
+++ SharpMod.Node/app.js
@@ -346,28 +346,28 @@ function parseMessage(message, emotes) {
 	return message;
 };
 
-// Quit when all windows are closed.
 app.on("window-all-closed", function() {
-	if(process.platform != "darwin") {
-		app.quit();
-	}
+	app.quit();
 });
 
-// This method will be called when atom-shell has done everything
-// initialization and ready for creating browser windows.
 app.on("ready", function() {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.openDevTools();
+	mainWindow = new BrowserWindow({
+		"min-width": 400,
+		width: 800,
+		"min-height": 400,
+		height: 600,
+		center: true,
+		"node-integration": false,
+		show: false,
+		title: "SharpMod",
+		frame: true
+	});
 
-	// and load the index.html of the app.
-	mainWindow.loadUrl("http://127.0.0.1:18044");
-
-	// Emitted when the window is closed.
 	mainWindow.on("closed", function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
 		mainWindow = null;
 	});
+
+	//mainWindow.openDevTools();
+	mainWindow.loadUrl("http://127.0.0.1:18044");
+	mainWindow.show();
 });

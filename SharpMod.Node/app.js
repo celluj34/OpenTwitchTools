8602946b66dc96ae67bff97515d2bef7b4e1a07e
Joe Cellucci <celluj34@gmail.com>
2015-03-31 19:47:21 -0500

    move to atom-shell from node-webkit

--- SharpMod.Node/app.js
+++ SharpMod.Node/app.js
@@ -10,7 +10,10 @@
     compression = require("compression"),
     bodyParser = require("body-parser"),
     router = express.Router(),
-    client;
+    app = require("app"),
+    BrowserWindow = require("browser-window"),
+    client,
+    mainWindow;
 
 server.locals.ipAddress = "127.0.0.1";
 server.locals.port = 18044;
@@ -342,3 +345,29 @@ function parseMessage(message, emotes) {
 
 	return message;
 };
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+	if(process.platform != "darwin") {
+		app.quit();
+	}
+});
+
+// This method will be called when atom-shell has done everything
+// initialization and ready for creating browser windows.
+app.on("ready", function() {
+	// Create the browser window.
+	mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.openDevTools();
+
+	// and load the index.html of the app.
+	mainWindow.loadUrl("http://127.0.0.1:18044");
+
+	// Emitted when the window is closed.
+	mainWindow.on("closed", function() {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
+});

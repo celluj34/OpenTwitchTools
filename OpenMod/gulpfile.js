6305927f10329f42207b2d0a3af255cd0efe5ba6
Joe Cellucci <celluj34@gmail.com>
2015-06-26 22:22:24 -0500

    get gulp to work and add all libs because of it

--- OpenMod/gulpfile.js
+++ OpenMod/gulpfile.js
@@ -8,7 +8,7 @@ eval("var project = " + fs.readFileSync("./project.json"));
 
 var paths = {
     bower: "./bower_components/",
-    lib: "./" + project.webroot + "/lib/"
+    lib: "./assets/lib/"
 };
 
 gulp.task("clean", function(cb) {
@@ -17,8 +17,13 @@ gulp.task("clean", function(cb) {
 
 gulp.task("copy", ["clean"], function() {
     var bower = {
-        "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,woff2,eot}",
-        "jquery": "jquery/dist/jquery*.{js,map}"
+        "bootstrap": "bootstrap/dist/**/*.{css,js,map,ttf,svg,woff,woff2,eot}",
+        "jquery": "jquery/dist/jquery*.{js,map}",
+        "jquery.atwho": "jquery.atwho/dist/**/*.{css,js,map}",
+        "jquery.caret": "jquery.caret/dist/**/*.{css,js,map}",
+        "knockout": "knockout/dist/knockout*.{js,map}",
+        "select2": "select2/dist/**/*{css,js,map}",
+        "underscore": "underscore/underscore*.{js,map}"
     };
 
     for(var destinationDir in bower) {
@@ -26,3 +31,5 @@ gulp.task("copy", ["clean"], function() {
             .pipe(gulp.dest(paths.lib + destinationDir));
     }
 });
+
+gulp.task("default", ["clean"]);

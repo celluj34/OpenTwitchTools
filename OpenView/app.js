4b19927da2bbec27ada5a9a5f268030ce0988eb2
Joe Cellucci <celluj34@gmail.com>
2015-06-26 22:22:44 -0500

    initial commit of OpenView

---
+++ OpenView/app.js
@@ -0,0 +1,84 @@
+﻿var express = require("express"),
+    server = express(),
+    router = express.Router(),
+    path = require("path"),
+    _ = require("underscore"),
+    _s = require("underscore.string"),
+    request = require("request"),
+    bodyParser = require("body-parser"),
+    app = require("app"),
+    BrowserWindow = require("browser-window"),
+    mainWindow;
+
+//https://github.com/justintv/Twitch-API/blob/master/embedding.md
+//https://github.com/justintv/Twitch-API/blob/master/player.md
+
+server.locals.appName = "OpenView";
+server.locals.ipAddress = "127.0.0.1";
+server.locals.port = 18011;
+server.locals.startupUrl = _s.sprintf("http://%s:%s", server.locals.ipAddress, server.locals.port);
+server.locals.index = path.join(__dirname, "index.html");
+server.locals.database = path.join(__dirname, "assets", "database.json");
+server.locals.icon = path.join(__dirname, "assets", "images", "icon.png");
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(express.static(__dirname));
+
+router.route("/")
+    .get(function(req, response) {
+        response.sendFile(server.locals.index);
+    });
+
+router.route("/search")
+    .post(function(req, response) {
+        var url = _s.sprintf("https://api.twitch.tv/kraken/search/channels?q=%s", req.body.channel);
+
+        request(url, function(err, resp, body) {
+            var data = JSON.parse(body);
+
+            var channels = _.chain(data.channels)
+                .map(function(item) {
+                    return {
+                        id: item.name,
+                        name: item.name
+                    };
+                })
+                .value();
+
+            response.json(channels);
+        });
+    });
+
+server.use("/", router);
+
+var serverListener = server.listen(server.locals.port, server.locals.ipAddress);
+
+app.on("window-all-closed", function() {
+    app.quit();
+});
+
+app.on("ready", function() {
+    mainWindow = new BrowserWindow({
+        "min-width": 400,
+        "width": 800,
+        "min-height": 400,
+        "height": 600,
+        "center": true,
+        "node-integration": false,
+        "show": false,
+        "title": server.locals.appName,
+        "icon": server.locals.icon,
+        "resizeable": true
+    });
+
+    mainWindow.setMenu(null);
+
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
+
+    //mainWindow.openDevTools();
+    mainWindow.loadUrl(server.locals.startupUrl);
+    mainWindow.show();
+});
---
+++ OpenView/assets/index.js
@@ -0,0 +1,163 @@
+﻿$(function() {
+    $("#joinChannelModal").modal("show");
+
+    setupCustomControls();
+    initializeKnockout();
+});
+
+function setupCustomControls() {
+    $(".collapse.navbar-collapse").on("click", ".autoClose", function() {
+        $(".collapse.navbar-collapse").collapse("hide");
+    });
+
+    $(".collapse.navbar-collapse").on("click", ".channelClose", function() {
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+
+    $("[data-toggle='tooltip']").tooltip();
+
+    //$("#webview-control")[0].addEventListener("dom-ready", function() {
+    //    window.viewModel.TokenAuthLoading(false);
+    //});
+    
+    $("#newChannel").select2({
+        ajax: {
+            delay: 200,
+            dataType: "json",
+            url: "/search",
+            data: function (params) {
+                return {
+                    channel: params.term,
+                    page: params.page
+                };
+            },
+            method: "POST",
+            processResults: function (data) {
+                return {
+                    results: data
+                };
+            }
+        },
+        allowClear: true,
+        minimumInputLength: 4,
+        placeholder: {
+            name: "Search",
+            id: null
+        },
+        templateResult: function (channel) {
+            return channel.name;
+        },
+        templateSelection: function (channel) {
+            return channel.name;
+        },
+        escapeMarkup: function (markup) {
+            return markup;
+        },
+        id: function (channel) {
+            return channel.id;
+        }
+    });
+}
+
+function initializeKnockout() {
+    var channelViewModel = function(data) {
+        var self = this;
+
+        //channel static properties
+        self.Name = data;
+        self.Brand = "#" + data;
+
+        //observable properties
+        self.Selected = ko.observable(false);
+    };
+
+    var windowViewModel = function() {
+        var self = this;
+        
+        //obs
+        self.LoginSelectedChannel = ko.observable();
+
+        //chat information
+        self.Channels = ko.observableArray();
+
+        //computed values
+        self.SelectedChannel = ko.computed(function() {
+            var selectedChannel = _.find(self.Channels(), function(channel) {
+                return channel.Selected();
+            });
+
+            if(_.isUndefined(selectedChannel)) {
+                return null;
+            }
+
+            return selectedChannel;
+        });
+
+        self.ChannelIsSelected = ko.computed(function() {
+            return !_.isNull(self.SelectedChannel());
+        });
+
+        self.Brand = ko.computed(function() {
+            if(self.ChannelIsSelected()) {
+                return self.SelectedChannel().Brand;
+            }
+
+            return "";
+        });
+
+        //functions
+        self.Select = function(channelName) {
+            _.each(self.Channels(), function(channel) {
+                channel.Selected(channel.Name === channelName);
+            });
+        };
+        
+        self.showJoinChannelModal = function() {
+            $("#joinChannelModal").modal("show");
+        };
+
+        self.joinChannel = function() {
+            if(self.LoginSelectedChannel()) {
+                $("#joinChannelModal").modal("hide");
+
+                var selectedChannel = self.LoginSelectedChannel().toLowerCase();
+
+                var matchingChannel = findMatchingChannel(selectedChannel);
+
+                if(!matchingChannel) {
+                    addChannel(selectedChannel);
+                }
+
+                self.Select(selectedChannel);
+            }
+        };
+
+        self.leaveChannel = function() {
+            self.Channels.remove(self.SelectedChannel());
+            var firstChannel = _.first(self.Channels());
+
+            if(!_.isUndefined(firstChannel)) {
+                self.Select(firstChannel.Name);
+            }
+        };
+
+        function findMatchingChannel(channelName) {
+            return _.find(self.Channels(), function(channel) {
+                return channel.Name === channelName;
+            });
+        }
+
+        function addChannel(selectedChannel) {
+            if(!selectedChannel) {
+                return;
+            }
+
+            var newChannel = new channelViewModel(selectedChannel);
+            self.Channels.push(newChannel);
+            self.Select(newChannel.Name);
+        }
+    };
+
+    window.viewModel = new windowViewModel();
+    ko.applyBindings(window.viewModel);
+}

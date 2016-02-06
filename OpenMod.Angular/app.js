09d0be9bc2daf06609668933de6e0473beb2e620
Joe Cellucci <celluj34@gmail.com>
2016-02-05 23:14:40 -0600

    minimum project to run some angular

---
+++ OpenMod.Angular/app.js
@@ -0,0 +1,41 @@
+﻿var express = require('express'),
+    server = express(),
+    app = require('app'),
+    BrowserWindow = require('browser-window'),
+    mainWindow;
+
+server.locals.ipAddress = '127.0.0.1';
+server.locals.port = 18011;
+server.locals.startupUrl = 'http://' + server.locals.ipAddress + ':' + server.locals.port + '/app/views/login.html';
+
+server.use(express.static(__dirname));
+
+server.listen(server.locals.port, server.locals.ipAddress);
+
+app.on('window-all-closed', function() {
+    app.quit();
+});
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        "min-width": 400,
+        "width": 800,
+        "min-height": 400,
+        "height": 600,
+        "center": true,
+        "node-integration": false,
+        "show": false,
+        "resizeable": true
+    });
+
+    //mainWindow.setMenu(null);
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+        delete mainWindow;
+    });
+
+    //mainWindow.openDevTools();
+    mainWindow.loadURL(server.locals.startupUrl);
+    mainWindow.show();
+});
---
+++ OpenMod.Angular/app/component.js
@@ -0,0 +1,12 @@
+﻿(function(app) {
+    app.AppComponent =
+        ng.core
+        .Component({
+            selector: 'open-mod',
+            template: '<h1>My First Angular 2-2 App</h1>'
+        })
+        .Class({
+            constructor: function() {
+            }
+        });
+})(window.app || (window.app = {}));

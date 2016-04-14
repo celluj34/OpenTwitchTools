9ceadd07eaae6c2235b664ebd9c0ef8f5dd0eda0
Joe Cellucci <celluj34@gmail.com>
2016-04-13 20:40:20 -0500

    sockets are working!! (again)

--- OpenMod.Angular/src/services/SocketService.js
+++ OpenMod.Angular/src/services/SocketService.js
@@ -5,53 +5,33 @@
         this._rootscope = $rootScope;
         this._socket = io.connect();
     }
-
-    //http://briantford.com/blog/angular-socket-io
     apply(callback) {
         this._rootscope.$apply(() => {
             callback.apply(this._socket, arguments);
         });
     }
     on(event, callback) {
-        //socket.on(eventName, function () {
-        //    var args = arguments;
-        //    $rootScope.$apply(function () {
-        //        callback.apply(socket, args);
-        //    });
-        //});
+        var socket = this._socket;
+        var rootScope = this._rootscope;
 
-        this._socket.on(event, () => {
+        socket.on(event, function() {
             var args = arguments;
-            this._rootscope.$apply(() => {
-                callback.apply(this._socket, args);
+            rootScope.$apply(function() {
+                callback.apply(socket, args);
             });
         });
-
-        //callback = this.apply(callback);
-
-        //this._socket.on(event, callback);
     }
     emit(event, data, callback) {
-        //socket.emit(eventName, data, function () {
-        //    var args = arguments;
-        //    $rootScope.$apply(function () {
-        //        if (callback) {
-        //            callback.apply(socket, args);
-        //        }
-        //    });
-        //});
+        var socket = this._socket;
+        var rootScope = this._rootscope;
 
-        this._socket.emit(event, data, () => {
+        socket.emit(event, data, function() {
             var args = arguments;
-            this._rootscope.$apply(() => {
+            rootScope.$apply(function() {
                 if(callback) {
-                    callback.apply(this._socket, args);
+                    callback.apply(socket, args);
                 }
             });
         });
-
-        //callback = this.apply(callback);
-
-        //this._socket.emit(event, data, callback);
     }
 }

0dbdc18274b4d717cad13eb5b9e34e8a485b73e5
Joe Cellucci <celluj34@gmail.com>
2016-04-13 23:54:12 -0500

    add a few placeholder spec files

--- OpenMod.Angular/_spec/references.js
+++ OpenMod.Angular/_spec/references.js
@@ -1,8 +1,1 @@
-﻿/// <reference path="../js/angular.min.js" />
-/// <reference path="../js/jquery.min.js" />
-/// <reference path="../js/bootstrap.min.js" />
-
-/// <reference path="../app/register.js" />
-/// <reference path="../app/controllers/Login.js" />
-/// <reference path="../app/services/LoginService.js" />
-/// <reference path="../app/services/SocketService.js" />
+﻿/// <reference path="../app/scripts/app.min.js" />

134a0c33521f3a3b59bf05112de7a80993231740
Joe Cellucci <celluj34@gmail.com>
2016-04-15 19:45:27 -0500

    submit-credentials calls new authentication

--- OpenMod.Angular/src/controllers/LoginController.js
+++ OpenMod.Angular/src/controllers/LoginController.js
@@ -10,6 +10,7 @@
         this.username = null;
         this.password = null;
         this.remember = null;
+        this.errors = [];
         this.loadingPromise = null;
         this.loaded = false;
     }
@@ -35,17 +36,27 @@
         this.loaded = true;
     }
     submitCredentials() {
+        this.errors.splice(0, this.errors.length);
+
         const data = {
             username: this.username,
             password: this.password,
             remember: this.remember
         };
 
-        this._loginService.submitCredentials(data, () => this.loginSuccessful());
+        this._loginService.submitCredentials(data, (result) => this.loginSuccessful(result));
     }
-    loginSuccessful() {
-        console.log('loginSuccessful()');
-        //this._state.go('chat');
+    loginSuccessful(result) {
+        if(result.isValid) {
+            alert('go to chat');
+            //this._state.go('chat');
+        }
+        else {
+            this.errors.push(result.error);
+        }
+    }
+    closeAlert(index) {
+        this.errors.splice(index, 1);
     }
 
     //openModal() {
